9981a45dc7f24cd90b93450e779383cc0622861b
aimbrouksuka <1thermaltake2705@gmail.com>
2023-12-06 23:55:26 +0300

    Стилизация карты user, отображение подробной информации о user из бд

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -10,9 +10,11 @@ const addButton = document.querySelector('.articles-addItem__btn')
 const closeInputBtn = document.querySelector('.articles-addItem-inputBlock__trash')
 const dataItems = document.querySelector('.articles-data-items')
 let newData = ''
-
+let newCard = ''
 
 const popup = document.querySelector('.popup')
+const card = document.querySelector('.popup-card')
+
 
 gsap.registerPlugin(Flip)
 
@@ -25,7 +27,7 @@ let counter = 1
 //656a1954322ca87e4bf35b9e
 
 users().then(({data}) => {
-
+    console.log(data)
     data.forEach(user => {
         newData += renderHTML(user)
         dataItems.innerHTML = newData;
@@ -36,14 +38,17 @@ users().then(({data}) => {
 
         item.addEventListener('click', () => {
             popup.classList.add('open')
-            console.log("test")
+            data.forEach(user => {
+                if (user._id === item.id) {
+                    newCard = renderCardHTML(user)
+                    card.innerHTML = newCard
+                }
+            })
         })
     })
-    console.log(dataItem)
 })
-
 const renderHTML = (user) => {
-    return `<div class="articles-data-items-item">${user.surname + ' ' + user.name + ' ' + user.patronymic}
+    return `<div class="articles-data-items-item" id="${user._id}">${user.surname + ' ' + user.name + ' ' + user.patronymic}
                         <div class="articles-data-items-item__trash">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="24" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
@@ -52,6 +57,27 @@ const renderHTML = (user) => {
                         </div>
                     </div>`
 }
+
+const renderCardHTML = (user) => {
+    return `<div class="popup-card__photo"></div>
+        <p class="popup-card__FIO">${user.surname + " " + user.name}<br> ${user.patronymic}</p>
+        <div class="popup-card-details">
+            <p>Возраст:</p>
+            <div class="popup-card-details-item">${user.age}</div>
+            <p>id:</p>
+            <div class="popup-card-details-item">${user._id}</div>
+        </div>
+        <div class="popup-card-btns">
+            <button class="popup-card-btns__refactor">Редактировать</button>
+            <button class="popup-card-btns__delete">Удалить</button>
+        </div>`
+}
+
+popup.addEventListener('click', ({ target }) => {
+    if (!target.closest('.popup-card')) {
+        popup.classList.remove('open')
+    }
+})
 
 number.addEventListener("change", ({ target: { value } }) => {
     counter = value
